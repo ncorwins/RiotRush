1c60b2ce8adfa8f9eac4e2b84d393138796e008f
ncorwins <112829556+ncorwins@users.noreply.github.com>
2022-09-04 19:15:37 -0700

    v4

--- RiotRush2/RiotRush2/ContentView.swift
+++ RiotRush2/RiotRush2/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+class NSMutableArray : NSArray {}
+
 struct ContentView: View {
     @State private var s1 = ""
     @State private var s2 = ""
@@ -24,153 +26,101 @@ struct ContentView: View {
     @State private var betsize = 1
     @State private var helpText = ""
     @State private var totalWin = 0
+    @State private var slots: [String] = ["","","","","","","","","",""]
     
     
     var body: some View {
         VStack(alignment: .center, spacing: 30) {
             VStack(alignment: .center, spacing: 20) {
-            Button(action: {
-                totalWin = 0
-                if (betsize < tokens+1) {
-                    helpText = ""
-                    basicLabel = ""
-                    let dictionary = ["1": "apple",
-                                      "2": "lemon",
-                                      "3": "donut",
-                                      "4": "cherry",
-                                      "5": "blueberries"]
-                    
-                    s1 = String(Int.random(in: 1..<6))
-                    s2 = String(Int.random(in: 1..<6))
-                    s3 = String(Int.random(in: 1..<6))
-                    s4 = String(Int.random(in: 1..<6))
-                    s5 = String(Int.random(in: 1..<6))
-                    s6 = String(Int.random(in: 1..<6))
-                    s7 = String(Int.random(in: 1..<6))
-                    s8 = String(Int.random(in: 1..<6))
-                    s9 = String(Int.random(in: 1..<6))
-                    
-                    s1 = dictionary[s1] ?? ""
-                    s2 = dictionary[s2] ?? ""
-                    s3 = dictionary[s3] ?? ""
-                    s4 = dictionary[s4] ?? ""
-                    s5 = dictionary[s5] ?? ""
-                    s6 = dictionary[s6] ?? ""
-                    s7 = dictionary[s7] ?? ""
-                    s8 = dictionary[s8] ?? ""
-                    s9 = dictionary[s9] ?? ""
-                    
-                    // CHECK TOP HORIZONTAL
-                    if (s1 == s2) {
-                        if (s2 == s3) {
-                            tokens += betsize*3
-                            totalWin = totalWin + betsize*3
-                        }
-                    }
-                    
-                    // CHECK MIDDLE HORIZONTAL
-                    if (s4 == s5) {
-                        if (s5 == s6) {
-                            tokens += betsize*3
-                            totalWin = totalWin + betsize*3
-                        }
-                    }
-                    
-                    // CHECK BOTTOM HORIZONTAL
-                    if (s7 == s8) {
-                        if (s8 == s9) {
-                            tokens += betsize*3
-                            totalWin = totalWin + betsize*3
-                        }
-                    }
-                    
-                    // CHECK 1st COLUMN
-                    if (s1 == s4) {
-                        if (s4 == s7) {
-                            tokens += betsize*3
-                            totalWin = totalWin + betsize*3
-                        }
-                    }
-                    
-                    // CHECK 2nd COLUMN
-                    if (s2 == s5) {
-                        if (s5 == s8) {
-                            tokens += betsize*3
-                            totalWin = totalWin + betsize*3
-                        }
-                    }
-                    
-                    // CHECK 3rd COLUMN
-                    if (s3 == s6) {
-                        if (s6 == s9) {
-                            tokens += betsize*3
-                            totalWin = totalWin + betsize*3
-                        }
-                    }
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    if (totalWin > 3) {
-                        basicLabel = "Big Win! (+" + String(totalWin) + ")"
-                    }
-                    else if (totalWin > 1) {
-                        basicLabel = "Winner! (+" + String(totalWin) + ")"
-                    }
-                    else {
+                Button(action: {
+                    totalWin = 0
+                    if (betsize < tokens+1) {
+                        helpText = ""
                         basicLabel = ""
-                        tokens -= betsize
-                    }
-                    
-                }
-                else {
-                    if (tokens == 0) {
-                        helpText = "Here are some more tokens"
-                        tokens += 15
+                        let dictionary = ["1": "apple",
+                                          "2": "lemon",
+                                          "3": "donut",
+                                          "4": "cherry",
+                                          "5": "blueberries"]
+                        slots = []
+                        for i in 0...8 {
+                            var num = (String(Int.random(in: 1..<6)))
+                            num = dictionary[num] ?? "0"
+                            slots.insert(num, at: i)
+                        }
+                        
+                        // CHECK HORIZONTALS
+                        for i in 0...8 {
+                            if (i == 0 || i == 3 || i == 6) {
+                                if (slots[i] == slots[i+1] && slots[i+1] == slots[i+2]) {
+                                    totalWin += totalWin + betsize*3
+                                    tokens += betsize*3
+                                }
+                            }
+                            if (i == 0 || i == 1 || i == 2) {
+                                if (slots[i] == slots[i+3] && slots[i+3] == slots[i+6]) {
+                                    totalWin += totalWin + betsize*3
+                                    tokens += betsize*3
+                                }
+                            }
+                        }
+                        
+                        
+                        if (totalWin > 3*betsize) {
+                            basicLabel = "Big Win! (+" + String(totalWin) + ")"
+                        }
+                        else if (totalWin > 1) {
+                            basicLabel = "Winner! (+" + String(totalWin) + ")"
+                        }
+                        else {
+                            basicLabel = ""
+                            tokens -= betsize
+                        }
+                        
                     }
                     else {
-                        helpText = "Decrease bet size to play!"
+                        if (tokens == 0) {
+                            helpText = "Here are some more tokens"
+                            tokens += 15
+                        }
+                        else {
+                            helpText = "Decrease bet size to play!"
+                        }
                     }
+                    
+                }, label: {
+                    Text("Spin!").fontWeight(.heavy).foregroundColor(.white).padding().background(Color.red).cornerRadius(15).font(.system(size: 36))
+                    
+                })
+            }
+                HStack(alignment: .center, spacing: 30) {
+                    Image(slots[0]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                    Image(slots[1]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                    Image(slots[2]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                }
+                HStack(alignment: .center, spacing: 30) {
+                    Image(slots[3]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                    Image(slots[4]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                    Image(slots[5]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                }
+                HStack(alignment: .center, spacing: 30) {
+                    Image(slots[6]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                    Image(slots[7]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                    Image(slots[8]).resizable()
+                        .frame(width: 64.0, height: 64.0)
+                }
+                HStack(alignment: .center, spacing: 30) {
+                    //Text("")
                 }
                 
-            }, label: {
-                Text("Spin!").fontWeight(.heavy).foregroundColor(.white).padding().background(Color.red).cornerRadius(15).font(.system(size: 36))
-                
-            })
-            
-            HStack(alignment: .center, spacing: 30) {
-                Image(s1).resizable()
-                    .frame(width: 64.0, height: 64.0)
-                Image(s2).resizable()
-                    .frame(width: 64.0, height: 64.0)
-                Image(s3).resizable()
-                    .frame(width: 64.0, height: 64.0)
-            }
-            HStack(alignment: .center, spacing: 30) {
-                Image(s4).resizable()
-                    .frame(width: 64.0, height: 64.0)
-                Image(s5).resizable()
-                    .frame(width: 64.0, height: 64.0)
-                Image(s6).resizable()
-                    .frame(width: 64.0, height: 64.0)
-            }
-            HStack(alignment: .center, spacing: 30) {
-                Image(s7).resizable()
-                    .frame(width: 64.0, height: 64.0)
-                Image(s8).resizable()
-                    .frame(width: 64.0, height: 64.0)
-                Image(s9).resizable()
-                    .frame(width: 64.0, height: 64.0)
-            }
-            HStack(alignment: .center, spacing: 30) {
-                //Text("")
-            }
-            
-        }
             HStack(alignment: .center, spacing: 50) {
                 Text("Tokens: " + String(tokens)).fontWeight(.heavy).foregroundColor(.white).padding().background(Color.purple).cornerRadius(15)
                 if (basicLabel == "") {
@@ -179,7 +129,7 @@ struct ContentView: View {
                 else {
                     Text(basicLabel).fontWeight(.heavy).foregroundColor(.white).padding().background(Color.blue).cornerRadius(10)
                 }
-
+                
             }
             HStack(alignment: .center, spacing: 20) {
                 Button(action: {
@@ -221,25 +171,8 @@ struct ContentView: View {
         
         
         
-        
-        
-        
-        
-        
-        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
